5fe5d60733df836358fccddb6759789be83188be
bineetNaidu <bineet.s.naidu@gmail.com>
2020-10-13 13:17:51 +0530

    -- add show all camps as INDEX -- show one camp as SHOW

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const Campground = require('./models/Campground');
 
 // *********** App Configuration ***********
 const app = express();
@@ -23,6 +24,16 @@ app.set(path.join(__dirname, 'views'));
 
 // Unmounting routes
 app.get('/', (_, res) => res.render('home'));
+
+app.get('/campgrounds', async (_, res) => {
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', { campgrounds });
+});
+
+app.get('/campgrounds/:id', async (req, res) => {
+  const campground = await Campground.findOne({ _id: req.params.id });
+  res.render('campgrounds/show', { campground });
+});
 
 // App Listeners
 app.listen(4242, () => console.log('YelpCamp Server has Started'));
