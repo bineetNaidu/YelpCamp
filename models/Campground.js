e66ebb61eac0882891df21a51a08fa4fc512214a
bineetNaidu <bineet.s.naidu@gmail.com>
2020-10-13 16:13:29 +0530

    -- updated model/Campground -- updated seeds/index -- updated show page -- add image field into Model

--- models/Campground.js
+++ models/Campground.js
@@ -3,9 +3,10 @@ const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
   title: String,
-  price: String,
+  price: Number,
   description: String,
   location: String,
+  image: String,
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
--- seeds/index.js
+++ seeds/index.js
@@ -29,6 +29,7 @@ const seedDB = async () => {
       description:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
       price,
+      image: "https://source.unsplash.com/collection/483251"
     });
     await camp.save();
   }
