f3468e333ab8cb22894d855d69e46eb7e10b6f7c
bineetNaidu <bineet.s.naidu@gmail.com>
2020-10-13 12:17:31 +0530

    --- connecting mongoose w/ database --- add Models

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-// Import
+// ************* Import ****************
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
-// App Configuration
+// *********** App Configuration ***********
 const app = express();
+
+// ? ***  DB connections ******
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'DB Connection Error'));
+db.on('open', () => console.log('>>>> DB Connected <<<<'));
+// ? ***  DB connections ******
 
 // Middlewares
 app.set('view engine', 'ejs');
 app.set(path.join(__dirname, 'views'));
 
 // Unmounting routes
-app.get('/', (req, res) => res.render('home'));
+app.get('/', (_, res) => res.render('home'));
 
 // App Listeners
 app.listen(4242, () => console.log('YelpCamp Server has Started'));
