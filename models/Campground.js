6bd3e4e3716fa2182d6e9495409f5993e3cb85b1
bineetNaidu <bineet.s.naidu@gmail.com>
2020-10-14 15:30:35 +0530

    add review Model && updated camp model

--- models/Campground.js
+++ models/Campground.js
@@ -7,6 +7,12 @@ const CampgroundSchema = new Schema({
   description: String,
   location: String,
   image: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Review',
+    },
+  ],
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
