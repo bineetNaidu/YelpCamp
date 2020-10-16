9bdaa80360825675150c6d971e34a47dc6c23415
bineetNaidu <bineet.s.naidu@gmail.com>
2020-10-16 14:49:34 +0530

    add controller to map && some tweaks

--- public/js/map.js
+++ public/js/map.js
@@ -6,6 +6,8 @@ const map = new mapboxgl.Map({
   zoom: 9,
 });
 
+map.addControl(new mapboxgl.NavigationControl());
+
 new mapboxgl.Marker()
   .setLngLat(campGeocode.geometry.coordinates)
   .setPopup(
